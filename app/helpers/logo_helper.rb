3a2b7c9b61704d2111d9f2678cf6470a9437b2aa
Michael Naurbjerg <michael@naurbjerg.dk>
2010-11-21 05:46:27 +0100

    Finished first cut of the User model

--- app/helpers/logo_helper.rb
+++ app/helpers/logo_helper.rb
@@ -1,11 +1,11 @@
 module LogoHelper
 
   def logo
-    logo = image_tag("logo.png", :alt => "Sample App", :class => "round", :title => self.title)
+    logo = image_tag("logo.png", :alt => "Sample App", :class => "round", :title => self.logotitle)
   end
 
   # Return a title on a per-page basis.
-  def title
+  def logotitle
     logo_title = "Sample App Logo"
     if @title.nil?
       logo_title
