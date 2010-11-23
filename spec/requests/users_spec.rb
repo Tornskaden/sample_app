f9a4e820124ce5846caf447032fd000177d52ae6
Michael Naurbjerg <michael@naurbjerg.dk>
2010-11-23 16:21:44 +0100

    Added user restrictions and gentle redirects

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -52,11 +52,6 @@ describe "Users" do
     describe "success" do
       it "should sign a user in and out" do
         integration_sign_in(Factory(:user))
-        #user = Factory(:user)
-        #visit signin_path
-        #fill_in :email,    :with => user.email
-        #fill_in :password, :with => user.password
-        #click_button
         controller.should be_signed_in
         click_link "Sign out"
         controller.should_not be_signed_in
